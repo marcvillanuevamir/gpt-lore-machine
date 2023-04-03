385732abc065c42ff54abb3ed91e0d1c8f244657
carlitoselmago <ccarbonell@gmail.com>
2023-04-03 22:32:55 +0200

    building

--- build.bat
+++ build.bat
@@ -1,7 +1,7 @@
 rmdir /s /q dist
 rmdir /s /q build
 call activate base
-python -m eel main.py frontend --onefile --noconsole --icon=frontend/favicon.ico
-::python -m eel template-sender.py frontend --onefile  --icon=frontend/favicon.ico
+::python -m eel main.py frontend --onefile --noconsole --icon=frontend/favicon.ico
+python -m eel main.py frontend --onefile  --icon=frontend/favicon.ico
 copy config.ini dist
 xcopy  /isvy "templates" "dist/templates"
