e36876bd470a626e4759bdb38af9c227edc99a16
carlitoselmago <ccarbonell@gmail.com>
2023-05-16 10:18:34 +0200

    minor updates

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -69,6 +69,33 @@ $( document ).ready(function() {
         eel.startChat(system,engine);
     });
 
+    function loadVoices() {
+        var voiceSelect = document.getElementById('voice');
+        // Fetch the available voices.
+          var voices = speechSynthesis.getVoices();
+        
+        // Loop through each of the voices.
+          voices.forEach(function(voice, i) {
+          // Create a new option element.
+              var option = document.createElement('option');
+          
+          // Set the options value and text.
+              option.value = voice.name;
+              option.innerHTML = voice.name;
+                
+          // Add the option to the voice selector.
+              voiceSelect.appendChild(option);
+          });
+      }
+      
+      // Execute loadVoices.
+      loadVoices();
+
+      // Chrome loads voices asynchronously.
+    window.speechSynthesis.onvoiceschanged = function(e) {
+        loadVoices();
+    };  
+
 
     //transcriber
     
